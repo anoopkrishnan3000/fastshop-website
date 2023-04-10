50c9d8f14df3eaebbd0a5d68bcd5f44fbd054fe8
anoop krishnan <anoopkrishnan3000@gmail.com>
2023-04-10 23:12:38 +0530

    commit

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,28 @@
-import { AppBar, Toolbar, styled }  from '@mui/material';
+import { AppBar, Toolbar, Box, styled }  from '@mui/material';
 
 // top app bar color changed here
 const StyledHeader = styled(AppBar)` 
   background: #413845;
+  height: 55px;
+
 `
-  
+const Component = styled(Box)`
+    margin-left: 12%;
+` 
 
 
 
 const Header = () => {
+
+    const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+ 
     return (
         <StyledHeader>
             <Toolbar>
-
+                <Component>
+                    <img src = {logoURL} alt="logo" style={{ width: 75}} />
+                    
+                </Component>
             </Toolbar>
         </StyledHeader>
     );
