60d10181ace517393c36e1ed6068f723501b6808
anoop krishnan <anoopkrishnan3000@gmail.com>
2023-04-10 22:51:22 +0530

    commit

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,20 @@
 import { AppBar, Toolbar, styled }  from '@mui/material';
 
-
-const StyledHeader = styled(AppBar)'
+// top app bar color changed here
+const StyledHeader = styled(AppBar)` 
+  background: #413845;
+`
   
-'
+
 
 
 const Header = () => {
     return (
-        <AppBar>
+        <StyledHeader>
             <Toolbar>
 
             </Toolbar>
-        </AppBar>
+        </StyledHeader>
     );
 }
 
